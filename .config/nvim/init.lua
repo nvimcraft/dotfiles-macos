6401dd8186c7bceb462373f7242c91ac7d4d3ba7
nvimcraft <260064684+nvimcraft@users.noreply.github.com>
2026-02-08 06:54:36 -0500

    maint(nvim): enforce LSP plugin load ordering

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,21 +1,34 @@
+--[=[
+Ordering is intentional.
+
+`core.lsp` evaluates LSP configs eagerly.
+`yamlls` requires schemastore.nvim at load time.
+With `vim.pack`, plugin modules must be loaded first.
+
+Do not move `core.lsp` above plugin requires.
+]=]
+
 require('config.options').setup()
 require('config.keymaps').setup()
 require('config.autocmds').setup()
 
+-- UI
 -- require('plugins.vague')
 require('plugins.everforest')
-require('plugins.conform')
-require('plugins.mason')
-require('plugins.luasnip')
-require('plugins.nvim-cmp')
-require('plugins.schemastore')
-require('plugins.plenary')
 require('plugins.mini-pairs')
+require('plugins.plenary')
+require('plugins.telescope')
 require('plugins.nvim-surround')
+require('plugins.conform')
 require('plugins.nvim-highlight-colors')
+
+-- LSP dependencies
+require('plugins.schemastore')
 require('plugins.gitsigns')
 require('plugins.octo')
-require('plugins.telescope')
 require('plugins.vim-dadbod')
+require('plugins.mason')
+require('plugins.nvim-cmp')
+require('plugins.luasnip')
 
 require('core.lsp')
